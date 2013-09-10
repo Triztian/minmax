39ab21052a3bbd0b0002f8923de9a7486537cbce
Tristian Azuara <tristian.azuara@gmail.com>
2013-09-10 11:51:26 -0700

    Add convenience functions to the State class

--- src/main/java/edu/uabc/ai/search/State.java
+++ src/main/java/edu/uabc/ai/search/State.java
@@ -22,4 +22,27 @@ public class State {
 
         return this.value.equals(((State)o).value);
     }
+
+    /**
+     *  Convenience function to create states.
+     *  @param v The value of the state
+     *  @param The created state
+     */
+    public static State S(Object v) {
+        return new State(v);
+    }
+
+    /**
+     * Convenience function to create a set of states.
+     *
+     * @param vs The values of each state.
+     * @return An array containing the created states.
+     */
+    public static State[] States(Object ... vs) {
+        State ss[]= new State[vs.length];
+        for(int i= 0; i<vs.length; i++)
+            ss[i]= new State(vs[i]);
+                
+        return ss;
+    }
 }
