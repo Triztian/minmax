062e3565cdefdf606926c7e7a996fb43c71c5a8d
Tristian Azuara <tristian.azuara@gmail.com>
2013-09-10 12:05:05 -0700

    [+] Set the costs for the paths in p2

--- src/main/java/edu/uabc/ai/p2/app/App.java
+++ src/main/java/edu/uabc/ai/p2/app/App.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 import edu.uabc.ai.search.*;
+import static edu.uabc.ai.search.State.S;
+import static edu.uabc.ai.search.State.States;
 import edu.uabc.util.StringUtil;
 
 public class App {
@@ -15,16 +17,27 @@ public class App {
 
     private static final List<String[]> CONNECTIONS= new ArrayList<String[]>() {{
         add(new String[] {"A", "B", "E", "H", "I"});
-        add(new String[] {"A", "C", "D", "H", "I"});
-        add(new String[] {"A", "D", "H", "I"});
-        add(new String[] {"A", "E", "F", "I"});
         add(new String[] {"A", "E", "G", "I"});
+        add(new String[] {"A", "C", "E", "I"});
         add(new String[] {"A", "D", "I"});
-        add(new String[] {"A", "H", "I"});
+    }};
+
+    private static final Map<State[], Integer> COSTS= new HashMap<State[], Integer>(){{
+        put(States("A", "B"), 50);
+        put(States("A", "C"), 20);
+        put(States("A", "D"), 50);
+        put(States("A", "E"), 50);
+        put(States("B", "C"), 35);
+        put(States("C", "D"), 35);
+        put(States("D", "E"), 40);
+        put(States("E", "F"), 60);
+        put(States("F", "G"), 25);
+        put(States("G", "H"), 50);
+        put(States("H", "I"), 70);
     }};
 
     public static void main(String args[]) {
-        Problem world= new World(CITIES, CONNECTIONS, "A");
+        Problem world= new World(CITIES, "A", CONNECTIONS, COSTS);
 
         Search search= new Search(world, new State("I"));
         State path[]= search.solve();
